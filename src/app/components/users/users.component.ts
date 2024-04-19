876043a43e8e97bfd762a3d693da6c48025a9859
amitjoshipune <amitjoshipune@gmail.com>
2024-04-19 17:25:03 +0530

    14. Send Data from the event Binding using $event reserved word in Angular - v1

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,15 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+
+
   allowNewUser :boolean = false;
- 
+  userCreatedStatus : string = 'No User is created';
+  userName:string = '';
+
+
   constructor() {
     console.log('ctor of users component');
     setTimeout(() => {
       this.allowNewUser = true;
     }, 3000);
   }
+
   ngOnInit(): void {
     console.log('ngonint of users component');
+  }
+
+  changeUserCreatedStatus = ()=>{
+    this.userCreatedStatus = 'User is created';
+  }
+
+  onUpdateUser = (event:Event)=> {
+    console.log(event);
   }
 }
